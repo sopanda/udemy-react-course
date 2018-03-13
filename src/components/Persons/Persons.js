6c4d38a16ac9d2a74102a72ac4c78b9cb74b3005
sopanda <hushchabogdan@gmail.com>
2018-03-13 19:14:30 +0100

    learned lifycycle Update hooks (triggered by Parent)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -16,6 +16,23 @@ class Persons extends Component {
       componentDidMount() {
         console.log("[Persons.js] - inside componentDidMount()");
       }
+    
+      componentWillReceiveProps(nextProps) {
+        console.log("[UPDATE Persons.js] - inside componentWillReceiveProps()", nextProps);
+      }
+
+      shouldComponentUpdate(nextProps, nextState) {
+        console.log("[UPDATE Persons.js] - inside shouldComponentUpdate()", nextProps, nextState);
+        return nextProps.persons !== this.props.persons;
+     }
+      
+      componentWillUpdate(nextProps, nextState) {
+        console.log("[UPDATE Persons.js] - inside componentWillUpdate()", nextProps, nextState);        
+     }
+
+      componentDidUpdate() {
+        console.log("[UPDATE Persons.js] - inside componentDidUpdate()");        
+      }
 
     render() {
         console.log("[Persons.js] - inside render()");
